4682d1d4b308e281e4b5265efa06bc3e2270cbfa
Anita Achu <anitatom20@gmail.com>
2020-12-15 15:48:49 +0100

    Add files via upload

--- blog/forms.py
+++ blog/forms.py
@@ -1,27 +1,22 @@
 from django import forms
-from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
-TAG_CHOICES =( 
-    ("1", "Private Law"), 
-    ("2", "Public Law"), 
-    ("3", "International Law"), 
-    ("4", "Jurisprudence"), 
-    ("5", "Others"), 
-) 
-  
 
-class InputForm(forms.Form):
-    Title = forms.CharField(max_length = 200) 
-    Tag_field = forms.ChoiceField(choices = TAG_CHOICES)
-    Author = forms.CharField(max_length = 50)
-    Body = forms.CharField(max_length = 2000, widget=forms.Textarea(),
-        help_text='Write here your blog post!'
-    )
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
 
-    def save(self, commit=True):
-        user = User.objects.create_user(
-            self.cleaned_data['fullname'],
-            self.cleaned_data['email'],
-            self.cleaned_data['password1']
-        )
-        return user
+    class Meta: 
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta: 
+        model = User
+        fields = ['username', 'email']
+
+
---
+++ blog/models.py
@@ -0,0 +1,2 @@
+from PIL import Image
+from django.contrib.auth.models import User
