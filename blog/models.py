e209b65d1b5cf8056483eeceb3592e4dd6548922
Anita Achu <anitatom20@gmail.com>
2020-12-15 16:01:55 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,18 @@
-from PIL import Image
+from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
+from PIL import Image
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default="The lawblog")
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
